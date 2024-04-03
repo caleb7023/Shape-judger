25d269e449dfaf177ed5b8aa9e4da8708d5d1fe6
caleb7023 <caleb70230@gmail.com>
2024-04-03 12:24:21 +0900

    first commit

---
+++ src/func.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# Author: caleb7023
+
+import numpy as np
+
+from random import randrange, getrandbits
+
+
+# Generate a array of X and Y pos of 128x128 array
+Size = np.arange(0, 128)
+pos_x_array, pos_y_array = np.meshgrid(Size, Size)
+pos_x_array = np.array(np.float64(pos_x_array))
+pos_y_array = np.array(np.float64(pos_y_array))
+del Size
+
+__all__ = [
+    "softmax"                ,
+    "create_ellipse_128x128" ,
+    "create_random_shape_img",
+]
+
+
+# \sigma(\vec{z} _ i) = \frac{e ^ {z _ i - z _ {max}} }{\sum ^ {K - 1} _ {j = 0} z _ j - z _ {max}}
+def softmax(array)->np.ndarray:
+
+    max_element = np.max(array)
+
+    e_powered_array = np.exp(array - max_element)
+
+    array_sum = 1 / np.sum(e_powered_array)
+
+    result = e_powered_array * array_sum
+
+    return result
+
+
+
+# Render ellipse.
+# The Pos1 should be smaller than Pos2.
+def create_ellipse_128x128(pos_1:tuple, pos_2:tuple) -> np.array:
+
+    global pos_x_array, pos_y_array
+
+    # Calc the center of the ellipse
+    ellipse_center = ((pos_1[0] + pos_2[0]) * 0.5, (pos_1[1] + pos_2[1]) * 0.5)
+
+    # Width ratio
+    width_ratio = ((pos_2[0] - pos_1[0]) / (pos_2[1] - pos_1[1]))
+
+    # The radius of the x axis
+    x_radius = (pos_2[0] - pos_1[0]) * 0.5
+
+    temp_pos_x_array, temp_pos_y_array = np.array(pos_x_array), np.array(pos_y_array)
+
+    temp_pos_x_array -= ellipse_center[0]
+    temp_pos_y_array -= ellipse_center[1]
+
+    # To create the ellipse as circle
+    temp_pos_y_array *= width_ratio
+
+    # Calc all the distance from Pos1
+    distance_array = np.sqrt(np.square(temp_pos_x_array) + np.square(temp_pos_y_array))
+
+    # If the distance were smaller than the X radius, the aug gonna be True
+    return distance_array < x_radius
+
+
+
+def create_random_shape_img() -> np.array:
+
+    # Pos1
+    pos_1 = (randrange(0, 100), randrange(0, 100))
+
+    # Pos2, will be bigger than Pos1 value
+    pos_2 = (randrange(pos_1[0] + 27, 127), randrange(pos_1[1] + 27, 127))
+
+    # if getrandbits(1) returns 1, its gonna create img of a rectangle.
+    # else its gonna return img of a ellipse.
+    is_rectangle = getrandbits(1)
+
+    if is_rectangle:
+
+        # Create a img with bool
+        Img = np.zeros((128 , 128) , bool)
+
+        # Get an img of a rectangle
+        Img[pos_1[0] : pos_2[0],
+            pos_1[1] : pos_2[1]] = True
+    else:
+        # Get an img of an ellipse
+        Img = create_ellipse_128x128(pos_1, pos_2)
+
+    for i in range(randrange(0, 3)):
+        Img = np.rot90(Img)
+
+    return Img, is_rectangle
---
+++ src/train.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+# Author: caleb7023
+
+import numpy as np
+
+# Either to show the lean progress or to debug
+
+import time
+
+import func
+
+
+
+def train(Img, is_rectrangle:bool)->bool:
+
+    global neuron_weights_ellipse, neuron_weights_rectangle
+
+    ellipse_score    = np.sum(neuron_weights_ellipse   * Img)
+    rectrangle_score = np.sum(neuron_weights_rectangle * Img)
+
+    softmax_output = func.softmax(np.array([ellipse_score, rectrangle_score]))
+
+    judged_shape_is_rectrangle = softmax_output[0] < softmax_output[1]
+
+    if judged_shape_is_rectrangle != is_rectrangle:
+        if judged_shape_is_rectrangle:
+            neuron_weights_ellipse   += Img * (softmax_output[0]-1) ** 2
+            neuron_weights_rectangle -= Img *  softmax_output[1]    ** 2
+        else:
+            neuron_weights_ellipse   -= Img *  softmax_output[0]    ** 2
+            neuron_weights_rectangle += Img * (softmax_output[1]-1) ** 2
+        return True
+
+    return False
+
+
+
+def main(save_to_disk:bool = True):
+
+    global neuron_weights_ellipse, neuron_weights_rectangle
+
+    neuron_weights_ellipse   = np.load("./train_data/neuron_weights/neuron_weights_ellipse.npy"  )
+    neuron_weights_rectangle = np.load("./train_data/neuron_weights/neuron_weights_rectangle.npy")
+
+    accuary_list = np.load("./train_data/accuary_list.npy")
+    
+    with open("./train_data/terms"      , "r") as f: terms      = int(f.read())
+    with open("./train_data/total_fails", "r") as f: total_fails = int(f.read())
+
+    while True:
+
+        start_time = time.time()
+
+        fails = 0
+        
+        for i in range(50000):
+
+            terms += 1
+
+            img, is_rectrangle = func.create_random_shape_img()
+
+            fails += train(img, is_rectrangle)
+            
+
+        total_fails += fails
+
+        accuary_list = np.append(accuary_list, fails * 0.00002)
+
+        ##########################
+        # Save datas to the disk #
+        ##########################
+        
+        if save_to_disk:
+
+            np.save("./train_data/neuron_weights/neuron_weights_ellipse"  , neuron_weights_ellipse)
+            np.save("./train_data/neuron_weights/neuron_weights_rectangle", neuron_weights_rectangle)
+            
+            np.save("./train_data/accuary_list", accuary_list)
+
+            with open("./train_data/terms"      , "w") as f: f.write(str(terms))
+            with open("./train_data/total_fails", "w") as f: f.write(str(total_fails))
+
+        ###############
+        # Print infos #
+        ###############
+
+        print("Terms:{0}, Total_fails:{1}, Acuracy:{2}%, Sec_per_10000_time:{3}".format(terms,
+                                                                                        total_fails,
+                                                                                        round(fails * 0.002, 1),
+                                                                                        round((time.time() - start_time) * 0.2, 3)))
+
+
+
+if __name__ == "__main__":
+    main(save_to_disk=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Im out of brain cells rn
