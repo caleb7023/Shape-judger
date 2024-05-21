cf31fb98c226ae4e140b4a0461c991be4adf2394
caleb7023 <caleb70230@gmail.com>
2024-05-21 17:50:02 +0900

    ㅤ

--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ from random import randrange, getrandbits
 
 # Render ellipse.
 # The Pos1 should be smaller than Pos2.
-def create_ellipse_128x128(pos_1:tuple, pos_2:tuple) -> np.array:
+def create_ellipse_128x128(pos_1:tuple, pos_2:tuple) -> np.ndarray:
     # Calculate the center of the ellipse
     center = ((pos_1[0] + pos_2[0]) // 2, (pos_1[1] + pos_2[1]) // 2)
     # Calculate the radii of the ellipse
@@ -26,7 +26,7 @@ def create_ellipse_128x128(pos_1:tuple, pos_2:tuple) -> np.array:
     ellipse_img = distance <= 1
     return ellipse_img.astype(bool)
 
-def create_random_shape_img() -> np.array:
+def create_random_shape_img() -> np.ndarray:
     # Pos1
     pos_1 = (randrange(0, 100), randrange(0, 100))
     # Pos2, will be bigger than Pos1 value
@@ -53,9 +53,10 @@ def main()->None:
     neural_network = ncl.NeuralNetwork(propatiy=prop)
     total_fails = 0
     total_terms = 0
+    EACH_TERMS = 100
     while True:
         fails = 0
-        for i in range(100):
+        for i in range(EACH_TERMS):
             # Create a random shape img
             img, is_rectangle = create_random_shape_img()
             # Flatten the img
@@ -65,12 +66,15 @@ def main()->None:
             # Get the result
             result = neural_network.value
             # Increase the fails if the result was wrong
-            if (result[1] < result[0]) != is_rectangle:
+            if (result[1] < result[0]) != is_rectangle or result[0] == result[1]:
                 fails += 1
             # Backward propagation
-            neural_network.backward_propagation(np.array([is_rectangle, not is_rectangle]))
-        total_terms += 100
+            target_value = np.array([is_rectangle, not is_rectangle])
+            neural_network.backward_propagation(target_value, 0.01)
+            if False: # debugging
+                print(f"{neural_network.value}, \n{target_value.astype(float)}\n")
+        total_terms += EACH_TERMS
         total_fails += fails
-        print(f"fails:{fails}, total_fails:{total_fails}, accuracy:{1-fails/100}, total_accuracy:{1-total_fails/total_terms}")
+        print(f"fails:{fails}, total_fails:{total_fails}, accuracy:{round(1-fails/EACH_TERMS, 4)}, total_accuracy:{round(1-total_fails/total_terms, 4)}")
 
 if __name__ == "__main__":main()
